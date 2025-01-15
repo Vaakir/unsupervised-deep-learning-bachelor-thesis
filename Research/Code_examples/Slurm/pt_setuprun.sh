8b5f2490c4d2137e41d33408fff5316d963fdab0
Jon Henrik <piehunter123@gmail.com>
2025-01-15 11:35:21 +0100

    Create pt_setuprun.sh

---
+++ Research/Code_examples/Slurm/pt_setuprun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --gres=gpu:0
+#SBATCH --partition=gpu 
+#SBATCH --time=02:15:00
+#SBATCH --job-name=pytorch_mnist_setup
+#SBATCH --output=mnist_setup.out
+ 
+# Set up environment
+uenv verbose cuda-11.4.4 cudnn-11.x-8.8.0 # must use this version!!
+uenv miniconda3-py39 # must use this version!!
+conda create -n pytorch_env -c pytorch pytorch torchvision numpy -y
+
+# Run the Python script that uses the GPU
+conda activate pytorch_env
+python -u pytorch_mnist.py
